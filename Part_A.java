053857f4bcc55de872f6094239ea399759882c73
Melisa Tanrikulu <melisa.tanrikulu@ug.bilkent.edu.tr>
2021-06-20 23:51:39 +0300

    Part a is done.

---
+++ Part_A.java
@@ -0,0 +1,22 @@
+import cs1.SimpleURLReader;
+
+/**
+ * Reads the contents of "http://www.cs.bilkent.edu.tr/~ozturk/cs102/housman.txt"
+ * and prints it and the number of lines it contains, on the console.
+ * @author Melisa Tanrikulu
+ * @version 20.06.2021
+ */
+public class Part_A {
+
+	public static void main(String[] args) {
+		// Initialization
+		SimpleURLReader page = new SimpleURLReader("http://www.cs.bilkent.edu.tr/~ozturk/cs102/housman.txt");
+		
+		// Prints the contents of the page
+		System.out.println(page.getPageContents());
+		
+		// Prints the number of lines it contains
+		System.out.println(page.getLineCount());
+	}
+
+}
